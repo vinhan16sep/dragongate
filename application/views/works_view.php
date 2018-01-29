c7d672b7280b91023db421829cd5008738cb8b40
minhtruongmato <truong.do@matocreative.vn>
2018-01-29 12:50:31 +0700

    update

--- application/views/works_view.php
+++ application/views/works_view.php
@@ -1,12 +1,4 @@
 <link rel="stylesheet" type="text/css" href="<?php echo site_url('assets/public/css/works.css') ?>">
-<script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.10";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
 <script>
     var is_search = false;
     var base_url = location.protocol + "//" + location.host + (location.port ? ':' + location.port : '');
